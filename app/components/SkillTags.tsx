b5526b847c9147c408c2a1b5cb51201ada0be02c
Daniel Yadghar <daniel072.dy@gmail.com>
2025-08-22 14:45:01 +0330

    Create SkillTags component

---
+++ app/components/SkillTags.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+interface Props {
+ backgroundColor: string;
+ textColor: string;
+ children: ReactNode;
+}
+const SkillTags = ({ children, textColor, backgroundColor }: Props) => {
+ return (
+  <div
+   className={`${textColor} ${backgroundColor} w-fit px-3 py-2 rounded-full`}
+  >
+   <p className="text-sm font-mono">{children}</p>
+  </div>
+ );
+};
+
+export default SkillTags;
